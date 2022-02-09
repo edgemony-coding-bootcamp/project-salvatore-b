839990e66140e2ce582f602d3b0bfee3b6d490c0
Floriana Domianello <floriana.domianello@gmail.com>
2022-02-09 16:41:41 +0100

    feat:  render prodotti-db logica

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,19 +1,62 @@
+import { useEffect, useState, useRef, useCallback } from 'react';
+import { db } from '../../firebase';
+import { addDoc, collection, onSnapshot, getDocs } from "firebase/firestore";
 
-
-
+  
 const Card = () => 
 {
+
+
+const [product, setProduct] = useState([]);
+
+
+useEffect( () => {
+    const getData = async () => {
+      const querySnapshot = await getDocs(collection(db, "prodotti"));
+const currentProdotti = querySnapshot.docs.map(doc => {
+        const obj = {
+          id: doc.id,
+          ...doc.data(),
+        };
+        return obj;
+      });
+      console.log(currentProdotti);
+      setProduct(currentProdotti);
+    }
+    getData()
+  },[]);
+
+
     return (
         <div>
-            <p>Categoria</p>
+{product.map((items, id) => (
+         <li key={items.id}>
+         <h4>{items.titolo}</h4>
+         <p>{items.prezzo}</p>
+         <img src={items.image} />
+         </li>
+       ))} 
+
+            {/* <p>Categoria</p>
             <img src="#" />
             <p>Titolo</p>
             <p>Descrizione Hover</p>
             <p>Money$$$</p>
             <p>Rating da prendere material icon</p>
-            <button>Buy Now</button>
+            <button>Buy Now</button> */}
         </div>
     );
 };
 
 export default Card
+
+
+// return (
+//     <div className="App">
+//       <h1>Hello guys</h1>
+//  <ul>
+       
+//       </ul>
+//     </div>
+//   )
+// }
