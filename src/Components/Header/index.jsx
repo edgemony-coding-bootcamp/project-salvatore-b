14ac77caa41dfc163c9fd49089fcfda76ab7165f
MarcoM94 <voldrak94@gmail.com>
2022-02-10 15:28:25 +0100

    feat: header

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,19 +1,58 @@
+import {Link} from "react-router-dom";
+import { Search, LocationOnOutlined } from '@mui/icons-material';
+import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import IconButton from '@mui/material/IconButton';
+import Badge from '@mui/material/Badge';
 
+import styles from "./Header.module.scss";
 
 
 const Header = () => {
     return (
-        <>
-            <div>
-                <img src="https://www.betonamit.com/wp-content/uploads/2020/09/amazon-logo-header.jpg" width="200" alt="" />
-                <span>Scegli il tuo indirizzo</span>
-                <input type="text"></input>
-                <button type="button">icona lente</button>
-                <a href="/">Ciao Accedi</a>
-                <a href="/">Politica Resi</a>
-                <a href="/">Carrello </a> {/*da sistemare com material ui */}
+        <header className={styles.head}>
+            <div className={styles.firstLine}>
+            <div className={styles.wrapper_header_section}>
+                <Link to="/">
+                    <img className={styles.imgLogoA} src="https://www.pinclipart.com/picdir/big/358-3584545_amazon-web-services-logo-png-transparent-svg-vector.png" alt="" />
+                </Link>
             </div>
-            <div>
+                <a>
+            <div className={styles.wrapper_header_section}>
+                <div className={styles.positionDiv}><LocationOnOutlined className={styles.positionOn}/>
+                <div>
+                <span>Ciao,</span>
+                <br/> Scegli il tuo indirizzo</div>
+                </div>
+            </div>
+                </a>
+                <div className={styles.searchDiv}>
+                <input className={styles.searchInput}  type="text"/>
+                <Search className={styles.iconSearch}/>
+                </div>
+                    <Link to="/login">
+                <div className={styles.wrapper_header_section}>
+                    <span>Ciao, Accedi</span> <br/> Account e liste 
+                    <ArrowDropDownIcon className={styles.arrowDropDown} fontSize="small"/>
+                </div>
+                    </Link>
+                    <a href="/">
+                <div className={styles.wrapper_header_section}>
+                    <span>Resi</span> <br/> e ordini
+                </div>
+                    </a>
+                <div className={styles.wrapper_header_section}>
+                    <a href="/">
+                    <IconButton aria-label="cart">
+                        <Badge badgeContent={1} className={styles.cartIcon}>
+                            <ShoppingBagOutlinedIcon fontSize="large" />
+                        </Badge>
+                    </IconButton>
+                    Carrello
+                     </a>
+                </div>
+            </div>
+            <div className={styles.secondLine}>
                 <a href="/">Amazon Choice</a>
                 <a href="/">Libri</a>
                 <a href="/">Eletronica</a>
@@ -21,7 +60,7 @@ const Header = () => {
                 <a href="/">Gaming</a>
                 <a href="/">Film e TV</a>
             </div>
-        </>
+        </header>
     )
 
 
