19fef7ab665bca177e864e228760d9bcf8194696
MarcoM94 <voldrak94@gmail.com>
2022-02-11 01:14:45 +0100

    feat: card styles

--- src/Components/Hero/index.jsx
+++ src/Components/Hero/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useRef } from "react";
 
 // import Fullscreen from "react-image-gallery";
 import styles from './Hero.module.scss';
@@ -11,10 +12,31 @@ import styles from './Hero.module.scss';
 // ];
 
 const Hero = () => {
+
+  const ref = (element, direction) => {
+    if (direction === "left") element.current.scrollLeft += 1170;
+    if (direction === "right") element.current.scrollLeft -= 1170;
+}
+
+const carousel = useRef();
+
+
   return (
-    <div className={styles.hero}>
-          <img src={"https://m.media-amazon.com/images/I/81OH+cHhEvL._SX3000_.jpg"} alt=""/>
+    <div className={styles.containerHero}>
+    <div className={styles.hero} ref={carousel}>
+    <div className={styles.heroImg}>
+      <div className={styles.imgRef}>
+            <img src={"https://m.media-amazon.com/images/I/81OH+cHhEvL._SX3000_.jpg"} alt=""/>
+            <img src={"https://m.media-amazon.com/images/I/81OH+cHhEvL._SX3000_.jpg"} alt=""/>
+      </div>
+      </div>
     </div>
+    <div className={styles.btnS}>
+                <button type="button" className={styles.btn} onClick={() => ref(carousel, "right") }>{"<"}</button>
+                <button type="button" className={styles.btn} onClick={() => ref(carousel, "left") }>{">"}</button>
+
+            </div>
+            </div>
   );
 };
 
