b691b73f8f61524a206d3356ad1387b66f6abf46
Francesca Belfiore <belfiorefrancesca94@gmail.com>
2022-02-16 11:19:15 +0100

    fix/duplicate-products

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -4,13 +4,15 @@ import { collection, getDocs } from "firebase/firestore";
 import { Rating } from "@mui/material";
 import styles from "./Card.module.scss";
 import { Context } from '../../Pages/Home';
-import {useStateValue} from "../../Libs/StateProvider";
+import { useStateValue } from "../../Libs/StateProvider";
 
 const Card = ({ category }) => {
   const [product, setProduct] = useState([]);
   // eslint-disable-next-line no-unused-vars
-  const [{basket}, dispatch] = useStateValue();
+  const [{ basket }, dispatch] = useStateValue();
   const { value } = useContext(Context);
+
+  const checkInclusi = (valore) => basket.find((prod) => prod.titolo.includes(valore));
 
   useEffect(() => {
     const getData = async () => {
@@ -41,8 +43,8 @@ const Card = ({ category }) => {
     });
   };
 
-  return (  
-      <div className={styles.Wrapper}>
+  return (
+    <div className={styles.Wrapper}>
       {product.map((items) => items.titolo.toLowerCase().includes(value.toLowerCase()) && (
         <div key={items.id} className={styles.Card}>
           <p className={styles.descCard}>{items.descrizione}</p>
@@ -50,19 +52,21 @@ const Card = ({ category }) => {
           <h4 className={styles.title_card}>{items.titolo}</h4>
           <div className={styles.PrRaBtn}>
             <div className={styles.Price_Rating}>
-            
-            <Rating name="half-rating-read" defaultValue={items.rating} precision={0.5} readOnly />
+
+              <Rating name="half-rating-read" defaultValue={items.rating} precision={0.5} readOnly />
               <span>{items.prezzo.toFixed(2)} €</span>
             </div>
             <div className={styles.btnAB}>
-              <button onClick={() => addToCart(items)} className={styles.btnAdd}>Aggiungi al carrello</button>
-               <button className={styles.btnBuy}><a href="/checkout">Acquista ora</a></button>
+              <button onClick={() => addToCart(items)} className={styles.btnAdd} disabled={checkInclusi(items.titolo)}>
+                {!checkInclusi(items.titolo) ? 'Aggiungi al carrello' : 'Già nel carrello'}
+              </button>
+              <button className={styles.btnBuy}><a href="/checkout">Acquista ora</a></button>
             </div>
           </div>
         </div>
       ))}
-      </div>
-    
+    </div>
+
   );
 };
 
