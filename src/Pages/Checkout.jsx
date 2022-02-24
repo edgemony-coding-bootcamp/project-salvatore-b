041d667b625bc9f316719206acd7e8e579739e12
Fablizio <gats200@hotmail.it>
2022-02-24 13:14:36 +0100

    fix checkout responsive

--- src/Pages/Checkout.jsx
+++ src/Pages/Checkout.jsx
@@ -8,17 +8,19 @@ const Checkout = () => {
     <>
       <Header />
       <div className={styles.Checkout__Card}>
+      <div className={styles.Checkout__Block}>
         <img
           src="https://png.pngtree.com/png-vector/20190114/ourlarge/pngtree-vector-delivery-truck-icon-png-image_314439.jpg"
           className={styles.Checkout__Icon}
           alt="checkout"
           loading="lazy"
         />
-        <h2>Grazie per aver Acquistato</h2>
-        <h4>Spero che la tua esperienza in snorlaxon sia stata fantastica!</h4>
+        <h2 className={styles.Checkout__text}>Grazie per aver Acquistato</h2>
+        <h4 className={styles.Checkout__text2}>Spero che la tua esperienza in Snorlaxon sia stata fantastica!</h4>
         <Link to="/">
           <button className={styles.Checkout__Button}>Torna Alla Home</button>
         </Link>
+        </div>
       </div>
 
       <Footer />
