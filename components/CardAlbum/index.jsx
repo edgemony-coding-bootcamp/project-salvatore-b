c35874b66f87e6688b7d0793ba3fe829a11ed9f9
Your Name <martiaruta@gmail.com>
2022-05-31 11:15:39 +0200

    card fixed

--- components/CardAlbum/index.jsx
+++ components/CardAlbum/index.jsx
@@ -34,13 +34,6 @@ export default function CardAlbum({ albumsData, inputSearchValue }) {
                   </div>
                 </a>
               </Link>
-              <div className={styles.img_container}>
-                <img></img>
-              </div>
-              <div className={styles.info_container}>
-                <h2>{album.title}</h2>
-                <p className={styles.year}>{album.year}</p>
-              </div>
             </div>
           ))}
     </div>
