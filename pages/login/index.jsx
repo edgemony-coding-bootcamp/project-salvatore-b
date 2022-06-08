a69b144419592d89466521d16a52cb7364100bde
Muriel Ingrassia <murielingrassia98@gmail.com>
2022-06-08 20:04:35 +0200

    Fix commit

--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -9,9 +9,6 @@ import styles from "./styles.module.scss";
 
 import { useRouter } from "next/router";
 
-
-
-
 const Login = () => {
   const router = useRouter();
 
@@ -28,16 +25,18 @@ const Login = () => {
         headers: { "Content-type": "application/json" },
         body: JSON.stringify(credentials),
       })
-        .then((response) => response.json())
-        .then((data) => {
-
-        setToken(data.accessToken);
-        
-        localStorage.setItem("userId", data.user.id)
-   
-        }
+        .then((response) => {
+          if (response.ok) { 
+            return response.json();
+           }
+           return Promise.reject(response); 
+        })
+        .then((data, status) => {
+            setToken(data.accessToken);
+            localStorage.setItem("userId", data.user.id)
+          }
         )
-
+        .catch(error => alert('Invalid credentials'));
     }
   // eslint-disable-next-line
   }, [credentials]);
