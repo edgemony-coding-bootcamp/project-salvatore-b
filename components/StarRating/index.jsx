992a04ee766e9af73fe22c1645f337083f1f2a94
Your Name <martiaruta@gmail.com>
2022-06-09 13:35:35 +0200

    rating stars fixed

--- components/StarRating/index.jsx
+++ components/StarRating/index.jsx
@@ -1,44 +1,50 @@
 import React, { useState, useEffect } from "react";
 import styles from "./styles.module.scss";
-import { AiFillStar, AiOutlineStar } from 'react-icons/ai';
+import { AiFillStar } from 'react-icons/ai';
 import { putRatingAlbum, putRatingPlaylist } from '../../utils';
 
 export default function StarRating({ album, playlist }) {
-  console.log('il tuo rating album è', album?.rating);
-  console.log('il tuo rating playlist è', playlist?.rating);
+
   const InitialRating = album ? album.rating : playlist.rating;
-  const [rating, setRating] = useState(InitialRating);
+
   const [hover, setHover] = useState(0);
-  
+  const [rating, setRating] = useState(InitialRating);
+
+
   useEffect(()=>{
     {album ?
-      putRatingAlbum(album?.id, {
-        rating: rating
-      })
+
+      putRatingAlbum(album?.id, {...album, rating: rating})
+
      :
-      putRatingPlaylist(playlist?.id, {
-        rating: rating
-      })
+
+      putRatingPlaylist(playlist?.id, {...playlist, rating: rating})
+
     }
   // eslint-disable-next-line
   },[rating])
 
+
+
+
   return (
     <div className={styles.star_rating}>
-      {[...Array(5)].map((star, index) => { index +=1;
+      {[...Array(5)].map((star, index) => {
+        index += 1;
         return (
           <button
             type="button"
             key={index}
-            className={index <= rating ? styles.on : styles.off}
-            onClick={() => setRating(index)}
+            className={index <= (hover || album?.rating || playlist?.rating) ? styles.on : styles.off}
+            onClick={() => {setRating(index)}}
             onMouseEnter={() => setHover(index)}
             onMouseLeave={() => setHover(rating)}
           >
-            <span className={styles.star}>{styles.off ? <AiFillStar size="1.2rem"/> : ''}</span>
+            <span className="star"><AiFillStar size="1.2rem"/></span>
           </button>
         );
       })}
     </div>
   );
+
 }
